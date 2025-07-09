e4254dc3bf0f21b0be91f5d1c32ec0be83c67af6
chenyu lin <2428028326@qq.com>
2025-07-09 12:18:36 +0800

    Update conflict_qa.py

--- data_process/conflict_qa.py
+++ data_process/conflict_qa.py
@@ -18,3 +18,18 @@ def conflict(data_path, max_results=10):
             "A": item['answer']
         })
     return QAs
+def conflict_mix(data_path, max_results=10):
+    """主处理函数，保持与原始定义完全一致"""
+    with open(data_path, 'r', encoding='utf-8') as file:
+        dataset = json.load(file)
+    
+    QAs = []
+    for item in dataset:
+        
+        
+        QAs.append({
+            "Q": item['question'],
+            "P": item['mixcontext'],
+            "A": item['answer']
+        })
+    return QAs
